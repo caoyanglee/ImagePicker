9eda5c96fb24c648cbd01af235e63a6e47c43a51
你需要一台永动机 <779213209@qq.com>
2019-03-19 11:02:17 +0800

    整理主页代码

--- app/src/main/java/io/weimu/www/imagepicker/activity/MainActivity.kt
+++ app/src/main/java/io/weimu/www/imagepicker/activity/MainActivity.kt
@@ -11,22 +11,40 @@ import com.weimu.library.view.ImageSelectorActivity
 import com.weimu.universalib.ktx.dip2px
 import com.weimu.universalib.ktx.getColorPro
 import com.weimu.universalview.core.recyclerview.decoration.GridItemDecoration
+import com.weimu.universalview.core.recyclerview.decoration.LinearItemDecoration
 import com.weimu.universalview.core.toolbar.StatusBarManager
 import com.weimu.universalview.ktx.requestPermission
 import io.weimu.www.imagepicker.R
 import io.weimu.www.imagepicker.fragment.adapter.ImageGridAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
-
-    private var recyclerView: RecyclerView? = null
-    private var mAdapter: ImageGridAdapter? = null
-    private var gridManager: GridLayoutManager? = null
-
-
     private val maxImageNumber = 9
     private val spanCount = 4
+
+    private val mAdapter: ImageGridAdapter by lazy {
+        ImageGridAdapter(this, maxImageNumber).apply {
+            //其他操作
+            this.imageActionListener = object : ImageGridAdapter.ImageActionListener {
+
+                override fun onItemClick(position: Int) {
+                    startActivity(PhotoViewPagerActivity.newInstance(this@MainActivity, position, this@apply.dataList))
+                }
+
+                override fun onItemDeleteClick(position: Int) {
+                    this@apply.removeItem(position)
+                }
+            }
+            //添加
+            this.onFooterClick = {
+                //ImagePicker.pickAvatar(this@MainActivity);
+                ImagePicker.pickImage(this@MainActivity, 9)
+                //ImagePicker.takePhoto(this@MainActivity, true);//使用摄像头
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,39 +61,18 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initRecyclerVIew() {
-        recyclerView = findViewById(R.id.id_RecyclerView)
-        mAdapter = ImageGridAdapter(this, maxImageNumber)
-        gridManager = GridLayoutManager(this, spanCount)
-        recyclerView!!.layoutManager = gridManager
-        recyclerView?.addItemDecoration(GridItemDecoration(spanCount, dip2px(8f), dip2px(8f)))
-        //设置Item增加、移除动画
-        //recyclerView.setItemAnimator(new NoAlphaItemAnimator());
-        recyclerView!!.adapter = mAdapter
-
-
-        mAdapter?.imageActionListener = object : ImageGridAdapter.ImageActionListener {
-
-            override fun onItemClick(position: Int) {
-                startActivity(PhotoViewPagerActivity.newInstance(this@MainActivity, position, mAdapter!!.dataList as ArrayList<String>))
-            }
-
-            override fun onItemDeleteClick(position: Int) {
-                mAdapter!!.removeItem(position)
-            }
-        }
-        mAdapter?.onFooterClick={
-            //                ImagePicker.pickAvatar(this@MainActivity);
-            ImagePicker.pickImage(this@MainActivity, 9)
-//                ImagePicker.takePhoto(this@MainActivity,true);//使用摄像头
+        this.id_RecyclerView.apply {
+            this.adapter = mAdapter
+            this.layoutManager = GridLayoutManager(this@MainActivity, spanCount)
+            this.addItemDecoration(GridItemDecoration(spanCount, dip2px(8f), dip2px(8f)))
         }
     }
-
 
     public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (resultCode == RESULT_OK && requestCode == ImageSelectorActivity.REQUEST_IMAGE) {
             val pics = data!!.getSerializableExtra(ImageSelectorActivity.REQUEST_OUTPUT) as ArrayList<String>
-            mAdapter!!.addData(pics)
+            mAdapter.addData(pics)
         }
     }
 }
